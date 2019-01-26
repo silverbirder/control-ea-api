9405e31ebc466ed0d83631de7bc602330075b727
shibamoto <silverbirder@gmail.com>
2019-01-26 20:53:10 +0900

    add mock data

--- p/fn.go
+++ p/fn.go
@@ -5,17 +5,46 @@ import (
 "net/http"
 )
 
+type Currency string
+
+const (
+ USD Currency = "USD"
+ CHF Currency = "CHF"
+ EUR Currency = "EUR"
+ GBP Currency = "GBP"
+ JPY Currency = "JPY"
+)
+
 type vip struct {
  StartDateTime string `json:"startDateTime"`
  EndDateTime string `json:"endDateTime"`
- Currency string `json:"currency"`
+ Currency Currency `json:"currency"`
+ RelatedCurrency []Currency `json:"relatedCurrency"`
  Title string `json:"title"`
 }
 
 func GetVipData(w http.ResponseWriter, r *http.Request) {
- v1 := vip{"2019/02/01 00:00:00", "2019/02/01 02:00:00", "USD", "なんか大変そうな発表1",}
- v2 := vip{"2019/02/01 04:00:00", "2019/02/01 08:00:00", "USD", "なんか大変そうな発表2",}
- v3 := vip{"2019/02/01 22:00:00", "2019/02/01 23:00:00", "USD","なんか大変そうな発表3",}
+ v1 := vip{
+  StartDateTime: "2019/02/01 00:00:00",
+  EndDateTime: "2019/02/01 02:00:00",
+  Currency: USD,
+  RelatedCurrency: []Currency{USD, CHF},
+  Title: "なんか大変そうな発表1",
+ }
+ v2 := vip{
+  StartDateTime: "2019/02/01 03:00:00",
+  EndDateTime: "2019/02/01 05:00:00",
+  Currency: USD,
+  RelatedCurrency: []Currency{USD, CHF, GBP},
+  Title: "なんか大変そうな発表2",
+ }
+ v3 := vip{
+  StartDateTime: "2019/02/01 22:00:00",
+  EndDateTime: "2019/02/01 23:00:00",
+  Currency: USD,
+  RelatedCurrency: []Currency{EUR, JPY},
+  Title: "なんか大変そうな発表3",
+ }
  vp := []vip{v1, v2, v3,}
  res, err := json.Marshal(vp)
  if err != nil {
