e2f734b13aaf6bcd8856c2c253e6b2eeba62d8c3
shibamoto <silverbirder@gmail.com>
2019-02-06 23:37:44 +0900

    GMT対応

--- p/fn.go
+++ p/fn.go
@@ -1,91 +1,91 @@
 package p
 
 import (
- "context"
- "encoding/json"
- "fmt"
- "time"
- "net/http"
+	"context"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"time"
 
- "cloud.google.com/go/datastore"
+	"cloud.google.com/go/datastore"
 )
 
 type vip struct {
- Currency string `json: "currency"`
- Date time.Time `json: "date"`
- Title string `json: "title"`
- Volatility int `json: "volatility"`
- IsClose bool `json:"isClose"`
- IsDelete bool `json:"isDelete"`
- RelatedCurrency []relatedCurrency `json:"relatedCurrency"`
+	Currency        string            `json: "currency"`
+	Date            time.Time         `json: "date"`
+	Title           string            `json: "title"`
+	Volatility      int               `json: "volatility"`
+	IsClose         bool              `json:"isClose"`
+	IsDelete        bool              `json:"isDelete"`
+	RelatedCurrency []relatedCurrency `json:"relatedCurrency"`
 }
 
 type response struct {
- Status string `json:"status"`
- Vips []vip `json:"vips"`
+	Status string `json:"status"`
+	Vips   []vip  `json:"vips"`
 }
 
 type relatedCurrency struct {
- Currency string `json:"currency"`
+	Currency string `json:"currency"`
 }
 
 func GetVipData(w http.ResponseWriter, r *http.Request) {
- if r.Method != "POST" {
-  http.Error(w, "not post", http.StatusInternalServerError)
-  return
- }
- if err := r.ParseForm(); err != nil {
-  http.Error(w, err.Error(), http.StatusInternalServerError)
-  return
- }
- symbol := r.FormValue("sy")
+	if r.Method != "POST" {
+		http.Error(w, "not post", http.StatusInternalServerError)
+		return
+	}
+	if err := r.ParseForm(); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	symbol := r.FormValue("sy")
 
- ctx := context.Background()
- dsClient, err := datastore.NewClient(ctx, "ma-web-tools")
- if err != nil {
-  fmt.Println(err)
- }
- now := time.Now()
- gmt := now.Add(-9 * time.Hour)
- startGmt := time.Date(gmt.Year(), gmt.Month(), gmt.Day(), 0, 0, 0, 0, time.Local)
- endGmt := time.Date(gmt.Year(), gmt.Month(), gmt.Day(), 23, 59, 59, 0, time.Local)
- var f []vip
- q := datastore.
-  NewQuery("VipData").
-  Filter("currency =", symbol[3:]).
-  Filter("volatility =", 3).
-  Filter("date >", startGmt).
-  Filter("date <", endGmt)
- _, err = dsClient.GetAll(ctx, q, &f)
- var b []vip
- q = datastore.
-  NewQuery("VipData").
-  Filter("currency =", symbol[:3]).
-  Filter("volatility =", 3).
-  Filter("date >", startGmt).
-  Filter("date <", endGmt)
- _, err = dsClient.GetAll(ctx, q, &b)
+	ctx := context.Background()
+	dsClient, err := datastore.NewClient(ctx, "ma-web-tools")
+	if err != nil {
+		fmt.Println(err)
+	}
 
- vp := append(f, b...)
+	jst, _ := time.LoadLocation("Asia/Tokyo")
+	jp := time.Now()
+	startJp := time.Date(jp.Year(), jp.Month(), jp.Day(), 0, 0, 0, 0, jst)
+	endJp := time.Date(jp.Year(), jp.Month(), jp.Day(), 23, 59, 59, 0, jst)
+	var f []vip
+	q := datastore.
+		NewQuery("VipData").
+		Filter("currency =", symbol[3:]).
+		Filter("volatility =", 3).
+		Filter("date >", startJp).
+		Filter("date <", endJp)
+	_, err = dsClient.GetAll(ctx, q, &f)
+	var b []vip
+	q = datastore.
+		NewQuery("VipData").
+		Filter("currency =", symbol[:3]).
+		Filter("volatility =", 3).
+		Filter("date >", startJp).
+		Filter("date <", endJp)
+	_, err = dsClient.GetAll(ctx, q, &b)
+	vp := append(f, b...)
 
- relatedCurrencyMap := map[string][]relatedCurrency{
-  "EUR": {{"GBP"}, {"CHF"}},
-  "GBP": {{"EUR"}, {"CHF"}},
-  "CHF": {{"EUR"}, {"GBP"}},
- }
- for i:= 0; i < len(vp); i++ {
-  vp[i].RelatedCurrency = relatedCurrencyMap[vp[i].Currency]
- }
- response := response{
-  Status:"ok",
-  Vips:vp,
- }
+	relatedCurrencyMap := map[string][]relatedCurrency{
+		"EUR": {{"GBP"}, {"CHF"}},
+		"GBP": {{"EUR"}, {"CHF"}},
+		"CHF": {{"EUR"}, {"GBP"}},
+	}
+	for i := 0; i < len(vp); i++ {
+		vp[i].RelatedCurrency = relatedCurrencyMap[vp[i].Currency]
+	}
+	response := response{
+		Status: "ok",
+		Vips:   vp,
+	}
 
- res, err := json.Marshal(response)
- if err != nil {
-  http.Error(w, err.Error(), http.StatusInternalServerError)
-  return
- }
- w.Header().Set("Content-Type", "application/json")
- w.Write(res)
+	res, err := json.Marshal(response)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(res)
 }
